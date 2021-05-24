04d70f3353dd6c9a19766a4d8cf3fff0b6640111
Nicolas Barragan <nicolas.barragan@upb.edu.co>
2021-05-24 14:37:33 -0500

    Generar mapa nivel 1

--- Assets/Scripts/BoardManager.cs
+++ Assets/Scripts/BoardManager.cs
@@ -4,6 +4,17 @@ using UnityEngine;
 
 public class BoardManager : MonoBehaviour
 {
+
+    [Header ("Tamaño nivel")]
+    public int columnas = 8; //numero de columnas del area en que se puede mover el personaje
+    public int filas = 8; //numero de filas del area en que se puede mover el personaje
+
+    [Header("Elementos generación nivel")]
+    private Transform boardHolder; //Referencia al objeto Mapa
+    [Header ("Baldosas")]
+    public GameObject[] floors; 
+    public GameObject[] outWalls;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -19,6 +30,42 @@ public class BoardManager : MonoBehaviour
     //Funcion que genera el nivel
     public void SetupScene()
     {
-        Debug.Log("SetupScene!");
+        //Debug.Log("SetupScene!");
+        //Generar Nivel
+        BoardSetup();
+    }
+
+    void BoardSetup()
+    {
+        //Crear un objeto que guarde todos los objetos creados en el nivel
+        boardHolder = new GameObject("Mapa").transform;
+
+        //Desplazarme por el area de juego(incluyendo los bordes)
+        //Desplazamiento a lo ancho
+        for(int x = -1; x < columnas + 1; x++) // x=-1 y x<columnas + 1 para que incluya los bordes externos
+        {
+            //Desplazamiento a lo largo
+            for(int y = -1; y < filas + 1; y++) // y=-1 y y<columnas + 1 para que incluya los bordes externos
+            {
+                //Tile (suelo) a instanciar en el nivel(mapa)
+                GameObject element = GetElement(floors);
+
+                //Cuando se encuentre en los bordes poner los muros
+                if(x==-1 || y==-1 || x==columnas || y == filas)
+                {
+                    //Tile (muro) a instanciar en el nivel(mapa)
+                    element = GetElement(outWalls);
+                }
+
+                //Poner el Tile en el nivel
+                GameObject instance = Instantiate(element, new Vector2(x, y), Quaternion.identity);
+                instance.transform.SetParent(boardHolder); //Guardar el objeto como hijo del objeto Mapa
+            }
+        }
+    }
+
+    GameObject GetElement(GameObject[] gameObjects)
+    {
+        return gameObjects[Random.Range(0, gameObjects.Length)];
     }
 }
