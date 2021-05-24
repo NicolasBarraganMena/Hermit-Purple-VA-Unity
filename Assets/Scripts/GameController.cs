58bd6e3e51fd3beb81466d3c7b6263321ca85d60
Nicolas Barragan <nicolas.barragan@upb.edu.co>
2021-05-24 14:02:20 -0500

    Crear código para controlar el juego y la generación de niveles.

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+
+    // Referencia al codigo generador de niveles
+    [Header("Generador Niveles")]
+    public BoardManager boardScript;
+
+    void Awake()
+    {
+        //Instanciar objeto con referencia al codigo generador de niveles
+        boardScript = GetComponent<BoardManager>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InitGame();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void InitGame()
+    {
+        //llamar al metodo Setup Scene en el codigo Board Manager (para generar el nivel)
+        boardScript.SetupScene();
+    }
+}
